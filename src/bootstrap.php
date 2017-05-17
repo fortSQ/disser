ebc9ebfffd16f01e186ae8c14e62768421ab7f3b
Dmitry V. Romanov <fortsq@gmail.com>
2017-05-17 13:57:38 +0300

    JS, tab and form

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,3 +5,5 @@
 define('PROJECT_PATH', realpath(__DIR__ . '/..'));
 
 $config = require_once PROJECT_PATH . '/config.php';
+
+$path = function ($path) { return '/disser/' . $path; };
--- web/index.php
+++ web/index.php
@@ -3,6 +3,14 @@
 
 <div class="row">
     <div class="col-md-8">
+        <ul class="nav nav-tabs" style="margin-bottom:-1px">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Размещённые <span class="badge badge-pill badge-default">216</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Ожидают размещения <span class="badge badge-pill badge-primary">53</span></a>
+            </li>
+        </ul>
         <table class="table">
             <thead>
             <tr>
@@ -36,9 +44,25 @@
     </div>
 </div>
 
+<h3 class="mb-4">Добавление нового товара</h3>
+<form>
+    <? foreach (['Название', 'Штрих-код', 'Ячейка'] as $item): ?>
+        <div class="form-group row">
+            <label class="col-md-2 col-form-label"><?= $item ?></label>
+            <div class="col-md-10">
+                <input type="email" class="form-control" placeholder="<?= $item ?>">
+            </div>
+        </div>
+    <? endforeach ?>
+    <div class="form-group row">
+        <div class="offset-md-2 col-md-10">
+            <button type="submit" class="btn btn-outline-primary"><i class="fa fa-save"></i> Сохранить</button>
+        </div>
+    </div>
+</form>
+
 <hr>
 <div>
-    <strong><i class="fa fa-fw fa-user"></i> Работник:</strong> Калинина Ксения Александровна<br>
     <strong><i class="fa fa-fw fa-cubes"></i> Собрано товаров (% от нормы):</strong> 1204/2048 (56%)<br>
     <strong><i class="fa fa-fw fa-clock-o"></i> Отработано часов:</strong> 3 ч. 28 мин.<br>
 </div>
