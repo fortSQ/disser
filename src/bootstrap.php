a976cdd5bda825ae3e94286021d43848c7b78c20
Dmitry V. Romanov <fortsq@gmail.com>
2017-05-24 18:26:39 +0300

    product prototypes

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,20 @@
 
 define('PROJECT_PATH', realpath(__DIR__ . '/..'));
 
+$path = function ($path) { return '/disser/' . $path; };
+
 $config = require_once PROJECT_PATH . '/config.php';
 
-$path = function ($path) { return '/disser/' . $path; };
+foreach (['A', 'B'] as $room) {
+    for ($i = 1; $i <= 40; $i++) {
+        for ($j = 1; $j <= 3; $j++) {
+            for ($k = 1; $k <= 5; $k++) {
+                if ($room == 'B' && $i > 10) {
+                    continue;
+                }
+
+                $config['cells'][] = "{$room}.{$i}.{$j}.{$k}";
+            }
+        }
+    }
+}
