c20f6ac28a3a2b0e79d9faba3ffe1b356a3283fa
Dmitry V. Romanov <fortsq@gmail.com>
2017-05-25 12:37:19 +0300

    Page pickup complete

--- web/pickup.php
+++ web/pickup.php
@@ -3,31 +3,29 @@
 
 <div class="row">
     <div class="col-md-8">
-        <ul class="nav nav-tabs" style="margin-bottom:-1px">
-            <li class="nav-item">
-                <a class="nav-link active" href="#">Размещённые <span class="badge badge-pill badge-default">216</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Ожидают размещения <span class="badge badge-pill badge-primary">53</span></a>
-            </li>
-        </ul>
         <table class="table">
             <thead>
             <tr>
                 <th>Товар</th>
                 <th>Штрих-код</th>
+                <th>Приоритет</th>
                 <th>Ячейка</th>
                 <th>Собран?</th>
             </tr>
             </thead>
             <tbody>
-            <? foreach ($config['goods'] as $product): ?>
+            <? foreach ($config['goods'] as $i => $product): ?>
                 <? $product = (object) $product ?>
                 <tr>
                     <td><?= $product->name ?></td>
                     <td><?= $product->barcode ?></td>
+                    <td><?= $product->priority ?></td>
                     <td><?= $product->cell ?></td>
-                    <td class="text-success"><i class="fa fa-check"></i></td>
+                    <td class="text-success">
+                        <? if ($i < 2): ?>
+                            <i class="fa fa-check"></i>
+                        <? endif ?>
+                    </td>
                 </tr>
             <? endforeach ?>
             </tbody>
@@ -43,23 +41,6 @@
         </figure>
     </div>
 </div>
-
-<h3 class="mb-4">Добавление нового товара</h3>
-<form>
-    <? foreach (['Название', 'Штрих-код', 'Ячейка'] as $item): ?>
-        <div class="form-group row">
-            <label class="col-md-2 col-form-label"><?= $item ?></label>
-            <div class="col-md-10">
-                <input type="email" class="form-control" placeholder="<?= $item ?>">
-            </div>
-        </div>
-    <? endforeach ?>
-    <div class="form-group row">
-        <div class="offset-md-2 col-md-10">
-            <button type="submit" class="btn btn-outline-primary"><i class="fa fa-save"></i> Сохранить</button>
-        </div>
-    </div>
-</form>
 
 <hr>
 <div>
